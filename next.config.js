3c83144ea458cc6689afa2e9e48b56fbe451a4b8
Austin Phan <austinphan2k2@gmail.com>
2024-01-01 17:40:32 -0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  
+   // Add basePath
+  basePath: '/github-pages',
  
   // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
   // trailingSlash: true,
