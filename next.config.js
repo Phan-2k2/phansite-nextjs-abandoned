5b1e01d0df7888dc9e56c36bbf2a7cbfd6f89ffa
Austin Phan <austinphan2k2@gmail.com>
2024-01-01 17:48:42 -0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -12,6 +12,7 @@ const nextConfig = {
    */
   images: {
     unoptimized: true,
+  }
  
   // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
   // trailingSlash: true,
