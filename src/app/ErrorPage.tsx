f3250d47a6cb2fcbd3b25c3569c484465ee171d7
Austin Phan <austinphan2k2@gmail.com>
2024-01-10 21:51:45 -0500

    i'm fixing colors because i'm stupid

--- src/app/ErrorPage.tsx
+++ src/app/ErrorPage.tsx
@@ -2,6 +2,19 @@ import Image from "next/image";
 import "./ErrorStyling.css"
 import { Button } from "@mui/material";
 import Link from "next/link";
+import { createTheme } from '@mui/material/styles';
+
+
+const theme = createTheme({
+    palette: {
+      primary: {
+        main: '#E3D026',
+        light: '#E9DB5D',
+        dark: '#A29415',
+        contrastText: '#242105',
+      },
+    },
+  });
 
 export default function ErrorPage () {
     //instead of actually writing the website I'm putting a confused travolta meme in here.
@@ -10,7 +23,7 @@ export default function ErrorPage () {
             <Image className="travoltagif" width="426" height="213" src="/phansite/images/confusedtravolta.gif" alt="Confused Travolta Meme"/>
             <h3>Uh oh. Something Went Wrong Here?</h3>
             <p>I literally wrote this page because I was procrastinating making actual pages &#128517;</p>
-            <Link href="/"><Button sx={{textTransform: 'none'}} className="Button">Take Me Back</Button></Link>
+            <Link href="/"><Button color="primary" size='large' sx={{textTransform: 'none'}} className="Button">Take Me Back</Button></Link>
         </div>
     )
 }
