ec5dcdfe7ed37dcb5ea27e53ab9c6ef48343b7c1
Austin Phan <austinphan2k2@gmail.com>
2024-01-10 20:47:22 -0500

    retest for gh pages with fixed page link

--- src/app/ErrorPage.tsx
+++ src/app/ErrorPage.tsx
@@ -7,7 +7,7 @@ export default function ErrorPage () {
     //instead of actually writing the website I'm putting a confused travolta meme in here.
     return(
         <div className="errorpagemain">
-            <img className="travoltagif" src="phansite/images/confusedtravolta.gif" alt="Confused Travolta Meme"/>
+            <img className="travoltagif" src="/phansite/images/confusedtravolta.gif" alt="Confused Travolta Meme"/>
             <h3>Uh oh. Something Went Wrong Here?</h3>
             <p>I literally wrote this page because I was procrastinating making actual pages &#128517;</p>
             <Link href="/"><Button className="errorbutton">Take Me Back</Button></Link>
