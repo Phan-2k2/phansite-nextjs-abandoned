a8f07445189581fe317aa942700c12603520bb60
Phan-2k2 <austinphan2k2@gmail.com>
2024-01-12 16:36:12 -0500

    working on mainpage now

--- src/app/mainpage/WelcomeSpread.tsx
+++ src/app/mainpage/WelcomeSpread.tsx
@@ -1,7 +1,19 @@
-import Image from 'next/image'
+'use client'
+import './WelcomeSpread.css'
+import AnimatedText from './AnimatedText'
+import GetWindowDimensions from '../helperfunctions/GetWindowDimensions';
 
 export default function WelcomeSpread() {
+    const { width, height } = GetWindowDimensions();
+    console.log(width, height)
     return(
-        <h1>Hi! I&apos;m Austin :D</h1>
+        <div className='welcomeSpreadMain'>
+            <img className='welcomeProfileImage' src="/images/anime-profile-pic-transparent.png" alt='Austin!'/>
+            <div className='welcomeSpreadText'>
+                <p className='welcomeStaticText'>Hi, I&apos;m <p className='name'>&nbsp;Austin&nbsp;</p> :D</p>
+                <AnimatedText/>
+            </div>
+
+        </div>
     )
 }
