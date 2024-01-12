a786f7898bcee8d5f060dfc382c6381aee33553b
Austin Phan <austinphan2k2@gmail.com>
2024-01-11 23:18:04 -0500

    idk lol am i doing a good navbar

--- src/app/mainpage/NavBar.tsx
+++ src/app/mainpage/NavBar.tsx
@@ -1,10 +1,21 @@
 import Link from "next/link"
+import './navBar.css'
 
 export default function NavBar(){
     return(
         <div>
-            <h3>Here&apos;s a navbar</h3>
-            <Link href="/projectpages">BANANANA</Link>
+            <div className="navBar">
+                <div className="navBarLogo">
+                    <p>austin.phan</p>
+                </div>
+                <div className="navBarLinks">
+                    <Link className="navBarLink" href="/projectpages">About Me</Link>
+                    <Link className="navBarLink" href="/projectpages">Projects</Link>
+                    <Link className="navBarLink" href="/projectpages">Experience</Link>
+                    <Link className="navBarLink" href="/projectpages">Contact</Link>
+                </div>
+            </div>
+            <div className="navBarSpacer"/>
         </div>
 
         )
